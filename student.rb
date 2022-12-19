22628041ddb0e1139ef2340ba72e3fc2a02b6697
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-12-19 13:47:31 -0300

    Add person, student and teacher classes

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_relative "person"
+class Student < Person
+  def initialize(age, name, parent_permission, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  attr_reader :classroom
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
