8c944fdcd648b99a9d9d82dcba2eea74c13243fe
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-12-19 14:27:09 -0300

    Fix errors

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require_relative 'person'
+
 class Student < Person
-  def initialize(age, name, parent_permission, classroom)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
