72bda87306badc53700a3aa5b765091c06d04f3d
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-12-20 18:18:51 -0300

    Fix errors

--- rental.rb
+++ rental.rb
@@ -1,20 +1,12 @@
 require_relative 'book'
 require_relative 'person'
 class Rental
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
-    @book = nil
-    @person = nil
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
   end
   attr_accessor :date, :book, :person
-
-  def add_book(book)
-    @book = book
-    book.rentals.push(self) unless book.rentals.include?(self)
-  end
-
-  def add_person(person)
-    @person = person
-    person.rentals.push(self) unless person.rentals.include?(self)
-  end
 end
